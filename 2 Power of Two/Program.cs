b653050d1974f3ddc8dcd11b7ddd4a867bd22cb9
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-09-22 15:24:07 +0300

    Add project files.

---
+++ 2 Power of Two/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace _2_Power_of_Two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+
+            long result = 1;
+            for (int i = 0; i <=num; i+=2)
+            {
+                if (i > 0)
+                {
+                    Console.Write(" , ");
+                }
+                Console.Write(result);
+                result = result * 2 * 2;
+            }
+            
+        }
+    }
+}
---
+++ Drawing with Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Drawing_with_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= num; row++)
+            {
+                Console.Write($"row: {row} ");
+                for (int column = 1; column <= num; column++)
+                {
+                    Console.Write(column + " ");
+                }
+                Console.WriteLine();
+            }   
+        }
+    }
+}
