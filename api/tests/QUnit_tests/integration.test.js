d77fd630b33c77f71d9084485cab327f6b46425c
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-18 20:44:10 +0300

    files added

--- api/tests/QUnit_tests/integration.test.js
+++ api/tests/QUnit_tests/integration.test.js
@@ -19,10 +19,10 @@ QUnit.module('user regiastration', ()=>{
         user.email=email
 
         //act
-        let response=await fetch(base_Url+'path'+{
+        let response=await fetch(base_Url+path+{
             method:'POST',
             headers:{
-                'content-type':'athorization/json'
+                'content-type':'application/json'
             },
             body: JSON.stringify(user)
         }) 
@@ -45,7 +45,35 @@ QUnit.module('user regiastration', ()=>{
 
         assert.ok(json.hasOwnProperty('_id'), 'id exist')
         userid=json['_id']
-        assert.strictEqual(typeof json._id, 'string', 'Property _id is a string')
+        assert.strictEqual(typeof json._id, 'string', 'Property _id is a string') 
+
+        sessionStorage.setItem('game-user', JSON.stringify(user))
+    })
+    QUnit.test('login testing', async(assert)=>{
+        //arrange
+        let path='/user/login'
+        
+        //act
+
+        let response= await fetch(base_Url+path+{
+            method:'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        let json=await response.json() 
+
+        //asert
+
+        assert.ok(response.ok, 'response is succesfull') 
+
+        assert.ok(json.hasOwnProperty('email'), 'email exists')
+        assert.equal(json[email], user.email, 'expected email')
+        assert.strictEqual(typeof json.email, 'string', 'Property email is a string')
+    
+
     })
 })
+
 
