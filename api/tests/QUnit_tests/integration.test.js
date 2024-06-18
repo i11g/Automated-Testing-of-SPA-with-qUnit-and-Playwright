b69d5e040213740ab332ef856a981ea8b1c8d403
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-18 15:05:00 +0300

    new files added

--- api/tests/QUnit_tests/integration.test.js
+++ api/tests/QUnit_tests/integration.test.js
@@ -1,1 +1,47 @@
-QUnit.config.reorder=false
+QUnit.config.reorder=false 
+const base_Url='http://localhost:3030'
+
+let user={
+    email:'',
+    password:'123456'
+}
+
+QUnit.module('user regiastration', ()=>{
+    QUnit.test('registration', async (assert) {
+        //arrange
+        let path='user/register'
+
+        let random=Math.floor(Math.random()*10000);
+        let email=`abv${random}@abv.bg`; 
+        user.email=email
+
+        //act
+        let response=await fetch(base_Url+'path'+{
+            method:'POST',
+            headers:{
+                'content-type':'athorization/json'
+            },
+            body: JSON.stringify(user)
+        }) 
+         
+        let json=await response.json();
+
+        //assert
+        assert.ok(response.ok, 'sucssesfull response'); 
+        assert.ok(json.hasOwnProperty('email'), 'email exist')
+        assert.equal(json['email'], user.email, 'expected mail')
+        assert.strictEqual(typeof json.email, 'string', 'Property mail is e string')
+
+        assert.ok(json.hasOwnProperty('password'), user.password, 'password exists')
+        assert.equal(json['password'], user.password, 'expected password')
+        assert.strictEqual(typeof json.password, 'string', 'password is a string')
+
+        assert.ok(json.hasOwnProperty('accessToken'), 'accessToken property exists')
+        token=json['accessToken']
+        assert.strictEqual(typeof json.accessToken, 'string', 'Property accessToken is a string') 
+
+        assert.ok(json.hasOwnProperty('_id'), 'id exist')
+        assert.strictEqual(typeof json._id, 'string', 'Property _id is a string')
+    })
+})
+
