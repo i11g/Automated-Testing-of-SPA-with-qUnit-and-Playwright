e22783fea640ad6354db20223404780bd1b48efe
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-20 06:48:40 +0300

    files changed

--- api/tests/Playwright_tests/e2e.test.js
+++ api/tests/Playwright_tests/e2e.test.js
@@ -1,4 +1,5 @@
-const {describe, test, beforeEach, afterEach, beforeAll, beforeEach, afterAll, expect}= require('@playwright/test');
+
+const {describe, test, beforeEach, afterEach, beforeAll, beforeEach, afterAll, expect} = require('@playwright/test');
 const {chromium} = require('palywright')
 
 const host = "http:/localhost:3000"; 
@@ -30,6 +31,35 @@ describe("e2e tests", ()=>{
     afterEach(async ()=> {
         await page.close();
         await context.close(); 
+    })
+    describe("authentication", async ()=>{
+        test("register with correct credentials makes corrext API call", async ()=>{
+            //arrange
+            await page.goto(host)
+            let random=Math.floor(Math.random()*10000);
+            user.mail=`abv${random}@abv.bg`
+            
+            //act
+            await page.click("text=Register")
+            await page.waitForSelector('form') 
+
+            await page.locator('#email').fill(usr.email)
+            await page.locator('#register-password').fill(user.password)
+            await page.locator('confirm-password').fill(user.confirmPass) 
+
+            let [response]= await Promise.all ([
+                page.waitForResponse(response=>response.url().includes('/user/register')&&response.status()===200),
+                page.click('[type=submit]')
+            ])
+
+            let userData=await response.json()
+
+            //arrange
+            await expect(response.ok).toBeTruthy();
+            expect(userData.email).toBe(user.email);
+            expect(userData.password).toBe(userData.password)
+
+        })
     })
 })
 
